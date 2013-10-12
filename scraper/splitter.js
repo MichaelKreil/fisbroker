2347a1aad1da6338c6d4b2046bc728cc39b48e72
Micho <github@michael-kreil.de>
2013-10-12 18:36:32 +0200

    cleaning up

--- scraper/splitter.js
+++ scraper/splitter.js
@@ -41,27 +41,27 @@ function scan(name, zoom) {
 		}
 	})
 
-	var todos1 = [];
+	var todos = [];
 	imageList.forEach(function (entry) {
 		var newFolder = '../data/tiles/'+name+'/'+newZoom+'/'+entry.x*factor+'/'+entry.y*factor;
 
 		if (!fs.existsSync(newFolder+'.png')) {
-			todos1.push('echo "'+zoom+'/'+entry.x+'/'+entry.y+'"');
+			//todos.push('echo "'+zoom+'/'+entry.x+'/'+entry.y+'"');
 
-			todos1.push(
+			todos.push(
 				'convert -crop 256x256 +repage ../data/big/luftbild2011/'+zoom+'/'+entry.x+'/'+entry.y+'.png '+newFolder+'_%d.png')
 			
 			var i = 0;
 			for (var dy = 0; dy < factor; dy++) {
 				for (var dx = 0; dx < factor; dx++) {
-					todos1.push('mv '+newFolder+'_'+i+'.png ../data/tiles/'+name+'/'+newZoom+'/'+(entry.x*factor+dx)+'/'+(entry.y*factor+dy)+'.png');
+					todos.push('mv '+newFolder+'_'+i+'.png ../data/tiles/'+name+'/'+newZoom+'/'+(entry.x*factor+dx)+'/'+(entry.y*factor+dy)+'.png');
 					i++;
 				}
 			}
 		}
 	})
 
-	fs.writeFileSync('./convert1_'+name+'.sh', todos1.join('\n'), 'utf8');
+	fs.writeFileSync('./script_split_'+name+'.sh', todos.join('\n'), 'utf8');
 }
 
 function ensureFolder(folder) {
